9bf19985b8bdeb00eb380b04ced1915c3de1f1ed
Jie Fan <lastomato@google.com>
2018-03-21 10:08:47 -0400

    Use default logger instead of printing to stderr directly.
    * Logger prints well formatted entries;
    * Logger prints stacktrace of throwables.

--- src/main/java/net/starschema/clouddb/jdbc/Logger.java
+++ src/main/java/net/starschema/clouddb/jdbc/Logger.java
@@ -1,43 +1,65 @@
 package net.starschema.clouddb.jdbc;
 
+import java.util.logging.Level;
+
+/**
+ * Wrapper class around {@link java.util.logging.Logger} to implement a log4j style interface.
+ */
 public class Logger {
 
-    static public
-    Logger getLogger(Object name) {
-        return new Logger();
+    private java.util.logging.Logger logger;
+
+    public static Logger getLogger(Object tag) {
+        return new Logger(objToMessage(tag));
     }
 
-    private Logger() {
+    private Logger(String tag) {
+        logger = java.util.logging.Logger.getLogger(tag);
+        // Print all logs.
+        logger.setLevel(Level.ALL);
     }
 
     public void debug(Object message) {
-        System.err.println(message);
+        logger.log(Level.FINEST, objToMessage(message));
     }
+
     public void debug(Object message, Throwable e) {
-        System.err.println(message + " " + e.toString());
+        logger.log(Level.FINE, objToMessage(message), e);
     }
+
     public void info(Object message) {
-        System.err.println(message);
+       logger.log(Level.INFO, objToMessage(message));
     }
+
     public void info(Object message, Throwable e) {
-        System.err.println(message + " " + e.toString());
+        logger.log(Level.INFO, objToMessage(message), e);
     }
+
     public void error(Object message) {
-        System.err.println(message);
+        logger.log(Level.SEVERE, objToMessage(message));
     }
+
     public void error(Object message, Throwable e) {
-        System.err.println(message + " " + e.toString());
+        logger.log(Level.SEVERE, objToMessage(message), e);
     }
+
     public void warn(Object message) {
-        System.err.println(message);
+        logger.log(Level.WARNING, objToMessage(message));
     }
+
     public void warn(Object message, Throwable e) {
-        System.err.println(message + " " + e.toString());
+        logger.log(Level.WARNING, objToMessage(message), e);
     }
+
     public void fatal(Object message) {
-        System.err.println(message);
+        error(message);
     }
+
     public void fatal(Object message, Throwable e) {
-        System.err.println(message + " " + e.toString());
+        error(message, e);
+    }
+
+    private static String objToMessage(Object message) {
+        return message == null ? "null" : message.toString();
     }
 }
